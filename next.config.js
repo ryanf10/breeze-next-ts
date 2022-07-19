761efd254745968dbad15f204e2603bd4bc4398b
ryanf10 <ryanfernaldy10@gmail.com>
2022-07-19 14:10:38 +0700

    chore: add axios

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
+  eslint: {
+    dirs: ['src'],
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
-
-module.exports = nextConfig
